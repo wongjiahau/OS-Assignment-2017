a284b7b0d865f499ce9a0d854bb0be7f66c09fc4
WJH <hou32hou@gmail.com>
2017-08-12 11:19:40 +0800

    Added file testing.sh -this file serve the purpose as a guideline on how to write bash script

---
+++ part-b-shell-script/testing.sh
@@ -0,0 +1,9 @@
+#!/bin/sh  
+#This is a comment 
+#If you cannot run this script, type the following command in terminal : 
+# chmod +x script_name.sh
+
+FIRST_ARGUMENT="$1"  #Remember dont include space between the equal sign, or else it won't work 
+echo "Hello, world! $FIRST_ARGUMENT"
+
+STRING1="This is a string variable"
