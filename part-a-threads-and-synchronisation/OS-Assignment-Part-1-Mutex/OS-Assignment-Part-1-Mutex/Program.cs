3b03e3f00f8a7aea658ef1e39c20142016e0f47d
WJH <hou32hou@gmail.com>
2017-08-17 10:48:09 +0800

    Added sample code for mutex

--- part-a-threads-and-synchronisation/OS-Assignment-Part-1-Mutex/OS-Assignment-Part-1-Mutex/Program.cs
+++ part-a-threads-and-synchronisation/OS-Assignment-Part-1-Mutex/OS-Assignment-Part-1-Mutex/Program.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OS_Assignment_Part_1_Mutex {
     class Program {
-        static void Main(string[] args) {
+        static Mutex _mutex;
+        static bool IsSingleInstance() {
+            try {
+                // Try to open existing mutex.
+                Mutex.OpenExisting("PERL");
+            }
+            catch {
+                // If exception occurred, there is no such mutex.
+                Program._mutex = new Mutex(true , "PERL");
+
+                // Only one instance.
+                return true;
+            }
+            // More than one instance.
+            return false;
+        }
+
+        static void Main() {
+            while (true) {
+                if (!Program.IsSingleInstance()) {
+                    Console.WriteLine("More than one instance"); // Exit program.
+                }
+                else {
+                    Console.WriteLine("One instance"); // Continue with program.
+                }
+                // Stay open.
+                Console.ReadLine();
+            }
         }
     }
+
 }
