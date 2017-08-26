4daf1673cf89232c00f10eccc56f498bc9e3b5a5
Jack109 <jacklau2010@live.com>
2017-08-27 01:11:08 +0800

    Update create_user.sh

--- part-b-shell-script/create_user.sh
+++ part-b-shell-script/create_user.sh
@@ -1,5 +1,13 @@
-echo "Enter your username : "
-read username
+create_user(){
+ USER=$1
+ shift;
 echo "I will create you a file called ${username}_file"
-mkdir -p $username/{guildeline,backup}
+mkdir -p /$username/{guildeline,backup}
+cp Readme.txt /$username/guildeline
+chmod 777 /$username/guildeline/Readme.txt
 echo "The end"
+}
+
+create_user abc
+
+
